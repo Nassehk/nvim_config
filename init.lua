11479895d1b29c925f424eb7b386e136bfe3825d
Nasseh Khodaie <nassehk@gmail.com>
2024-02-18 23:45:31 -0500

    set up lazy to load plagins from directories not from init.lua

--- init.lua
+++ init.lua
@@ -18,32 +18,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-     dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
-    {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
-}
 
-}
 
 local opts = {}
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
-require("catppuccin").setup()
-vim.cmd.colorscheme("catppuccin")
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
